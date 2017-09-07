e2cc825f430792ef12d8b5a789ac1087c3ad748a
Mauro Colella <mauro.ludovico.colella@gmail.com>
2017-09-07 18:41:43 +0800

    Add basic list to sidebar

--- src/containers/Sidebar/Sidebar.jsx
+++ src/containers/Sidebar/Sidebar.jsx
@@ -15,13 +15,14 @@ export default class Sidebar extends React.Component {
 	render() {
 		return (
 			<section className={styles.sidebar}>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultricies erat, at luctus lacus dapibus et. Nam eu molestie felis. Sed id pretium metus. Fusce a orci aliquet, gravida ante eget, consequat est. Donec pharetra eu urna vel molestie. Maecenas a lorem aliquam, vulputate dui sit amet, condimentum nisi. Vestibulum sit amet turpis laoreet, sodales libero venenatis, dignissim lorem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse imperdiet eros a blandit commodo. Suspendisse potenti. Aenean consequat ultricies ipsum in pulvinar. Morbi euismod risus vel nisl rhoncus, eleifend condimentum eros eleifend. Vivamus augue purus, sodales eu ante sit amet, lobortis iaculis ipsum. Maecenas sit amet augue quis nibh lobortis condimentum.
-				</p>
-				<br/>
-				<p>
-					Phasellus at quam dignissim, semper ligula id, consectetur velit. Fusce ex eros, auctor id est vel, molestie placerat erat. Praesent volutpat ligula interdum commodo placerat. Morbi at porta metus. Quisque turpis urna, auctor a ipsum a, commodo auctor dui. Suspendisse pulvinar enim arcu, sed facilisis tortor pretium eget. Nunc eu turpis ac eros condimentum dictum ut ac lorem. Mauris sodales egestas dolor, et molestie sapien porttitor ac. Cras massa nisl, dapibus eu diam sit amet, ornare dignissim magna.
-				</p>
+				<ul>
+					<li>
+						Lorem
+					</li>
+					<li>
+						Ipsum
+					</li>
+				</ul>
 			</section>);
 	}
 }
