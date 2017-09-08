afa4dd6acf4b9bc7eea6599f43375c2bdce02790
Mauro Colella <mauro.ludovico.colella@gmail.com>
2017-09-08 12:58:23 +0800

    Add react router v4

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Dashboard from './containers/Dashboard';
 export default class App extends Component {
 	render() {
 		return (
-			<div className={styles.page}>
+			<div className={styles.main}>
 				<Sidebar />
 				<Dashboard />
 			</div>
