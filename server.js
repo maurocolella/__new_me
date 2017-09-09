72525b06cd126576cf4574c04f3430a3415014a9
Mauro Colella <mauro.ludovico.colella@gmail.com>
2017-09-09 13:37:21 +0800

    Add ejs for base template

--- server.js
+++ server.js
@@ -6,20 +6,24 @@ var config = require('./webpack.config');
 var app = express();
 var compiler = webpack(config);
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.use(require('webpack-dev-middleware')(compiler, {
-  publicPath: config.output.publicPath
+	publicPath: config.output.publicPath
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+	res.render('index.ejs');
+	// res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, function(err) {
-  if (err) {
-    return console.error(err);
-  }
+	if (err) {
+		return console.error(err);
+	}
 
-  console.log('Listening at http://localhost:3000/');
+	console.log('Listening at http://localhost:3000/');
 });
