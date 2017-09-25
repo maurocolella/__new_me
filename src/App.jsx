1df6b56dda1682f5d81c14233e5c50a53ec892d0
Mauro Colella <mauro.ludovico.colella@gmail.com>
2017-09-25 10:16:15 +0800

    Add basic top level navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import cssreset from 'normalize.css';
 import typography from './assets/styles/typeplate.scss';
 import styles from './App.scss';
 
+import Navbar from './containers/Navbar';
 import Dashboard from './containers/Dashboard';
-import Sidebar from './containers/Sidebar';
 
 export default class App extends Component {
 	render() {
 		return (
 			<div className={styles.app}>
-				<Sidebar />
+				<Navbar/>
 				<Dashboard />
 			</div>
 		);
