386f83852974b65762dd4703adfcbe7fad3aeb5c
Mauro Colella <mauro.ludovico.colella@gmail.com>
2017-09-28 19:12:22 +0800

    Add basic footer cta

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './Footer.scss';
+import SunIcon from '../Icons/SunIcon';
 
 export default class Footer extends React.Component {
 	constructor(props) {
@@ -12,6 +13,9 @@ export default class Footer extends React.Component {
 		return (
 			<footer id="footer" className={styles.footer}>
 				<section className={styles.cover}>
+					<a href="#footer" className={styles.hireme}>
+						Hire Me <SunIcon className={styles.hireme__icon} />
+					</a>
 				</section>
 			</footer>
 		);
