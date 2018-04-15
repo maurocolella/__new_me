7c7efee8e01d377e6ee4584f220bcb88495bc575
Mauro Colella <mauro@mytenten.com>
2018-04-15 12:04:19 +0800

    Implement session storage cache on all endpoints

--- src/containers/SkillsPage/services.js
+++ src/containers/SkillsPage/services.js
@@ -1,14 +1,21 @@
 import 'isomorphic-fetch';
+import sscache from 'session-storage-cache';
 import deserialize from './model';
 
 async function getSkills() {
-  const response = await fetch('//api.mauro-colella.com/skills');
-  if (!response.ok) {
-    throw Error(response.statusText);
-  }
+  let data = sscache.get('skillsData');
 
-  const dataSet = await response.json();
-  return deserialize(dataSet);
+  if (!data) {
+    const response = await fetch('//api.mauro-colella.com/skills');
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+
+    const dataSet = await response.json();
+    data = deserialize(dataSet);
+    sscache.set('skillsData', data, 5);
+  }
+  return data;
 }
 
 export default getSkills;
