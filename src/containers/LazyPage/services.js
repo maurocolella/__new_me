210f9dff88624930e73b372cd7161332525f9421
Mauro Colella <mauro.ludovico.colella@gmail.com>
2018-05-09 21:55:10 +0800

    Connect resume endpoint

--- src/containers/LazyPage/services.js
+++ src/containers/LazyPage/services.js
@@ -1,195 +1,20 @@
 import 'isomorphic-fetch';
-import moment from 'moment';
 import sscache from 'session-storage-cache';
 import deserialize from './model';
 
 async function getResume() {
-  /*
-  TODO: implement
-  const response = await fetch('//api.mauro-colella.com/skills');
-  if (!response.ok) {
-    throw Error(response.statusText);
-  }
-
-  const dataSet = await response.json();
-  */
   let data = sscache.get('resumeData');
-  if (!data) {
-    const dataSet = {
-      data: [
-        {
-          id: '1',
-          type: 'resumeEntry',
-          attributes: {
-            company: 'ACME Inc',
-            description: 'Some good job.',
-            startDate: moment().format('l'),
-            endDate: moment().format('l'),
-          },
-          relationships: [
-            {
-              type: 'tasks',
-              data: [
-                {
-                  type: 'task',
-                  id: '1',
-                },
-                {
-                  type: 'task',
-                  id: '2',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          id: '2',
-          type: 'resumeEntry',
-          attributes: {
-            company: 'MEGA Inc',
-            description: 'Some really good job.',
-            startDate: moment().format('l'),
-            endDate: moment().format('l'),
-          },
-          relationships: [
-            {
-              type: 'tasks',
-              data: [
-                {
-                  type: 'task',
-                  id: '2',
-                },
-                {
-                  type: 'task',
-                  id: '1',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          id: '3',
-          type: 'resumeEntry',
-          attributes: {
-            company: 'ACME Inc',
-            description: 'Some good job.',
-            startDate: moment().format('l'),
-            endDate: moment().format('l'),
-          },
-          relationships: [
-            {
-              type: 'tasks',
-              data: [
-                {
-                  type: 'task',
-                  id: '1',
-                },
-                {
-                  type: 'task',
-                  id: '2',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          id: '4',
-          type: 'resumeEntry',
-          attributes: {
-            company: 'MEGA Inc',
-            description: 'Some really good job.',
-            startDate: moment().format('l'),
-            endDate: moment().format('l'),
-          },
-          relationships: [
-            {
-              type: 'tasks',
-              data: [
-                {
-                  type: 'task',
-                  id: '2',
-                },
-                {
-                  type: 'task',
-                  id: '1',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          id: '5',
-          type: 'resumeEntry',
-          attributes: {
-            company: 'ACME Inc',
-            description: 'Some good job.',
-            startDate: moment().format('l'),
-            endDate: moment().format('l'),
-          },
-          relationships: [
-            {
-              type: 'tasks',
-              data: [
-                {
-                  type: 'task',
-                  id: '1',
-                },
-                {
-                  type: 'task',
-                  id: '2',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          id: '6',
-          type: 'resumeEntry',
-          attributes: {
-            company: 'MEGA Inc',
-            description: 'Some really good job.',
-            startDate: moment().format('l'),
-            endDate: moment().format('l'),
-          },
-          relationships: [
-            {
-              type: 'tasks',
-              data: [
-                {
-                  type: 'task',
-                  id: '2',
-                },
-                {
-                  type: 'task',
-                  id: '1',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-      included: [
-        {
-          id: '1',
-          type: 'task',
-          attributes: {
-            description: 'Lorem ipsum.',
-          },
-        },
-        {
-          id: '2',
-          type: 'task',
-          attributes: {
-            description: 'Dolor amet.',
-          },
-        },
-      ],
-    };
 
+  if (!data) {
+    const response = await fetch('//api.mauro-colella.com/resume');
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+
+    const dataSet = await response.json();
     data = deserialize(dataSet);
     sscache.set('resumeData', data, 5);
   }
-
   return data;
 }
 
