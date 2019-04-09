1ba9450b7fdbe9f13a29dc728a7788a59df8160f
Mauro Colella <mauro.ludovico.colella@gmail.com>
2019-04-09 15:58:46 +0800

    Disable lazy loading on CV entries

--- src/components/ResumeEntry/ResumeEntry.jsx
+++ src/components/ResumeEntry/ResumeEntry.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import LazyLoad from 'react-lazyload';
+// import LazyLoad from 'react-lazyload';
 import moment from 'moment';
 import styles from './ResumeEntry.scss';
 
@@ -25,29 +25,27 @@ class ResumeEntry extends PureComponent {
     const dateFormat = 'MMMM YYYY';
 
     return (
-      <LazyLoad>
-        <article className={styles.entry}>
-          <h5 className={styles.entry__heading}>
-            <span>
-              {moment(startDate).format(dateFormat)}
-              {' '}
+      <article className={styles.entry}>
+        <h5 className={styles.entry__heading}>
+          <span>
+            {moment(startDate).format(dateFormat)}
+            {' '}
 -
-              {moment(endDate).format(dateFormat)}
-            </span>
-            <span style={{ textAlign: 'right' }}>
-              {title}
-            </span>
-          </h5>
-          <p>{description}</p>
-          <ul>
-            {tasks
-              && tasks.map(task => (
-                <li key={task.id}>{task.description}</li>
-              ))
-            }
-          </ul>
-        </article>
-      </LazyLoad>
+            {moment(endDate).format(dateFormat)}
+          </span>
+          <span style={{ textAlign: 'right' }}>
+            {title}
+          </span>
+        </h5>
+        <p>{description}</p>
+        <ul>
+          {tasks
+            && tasks.map(task => (
+              <li key={task.id}>{task.description}</li>
+            ))
+          }
+        </ul>
+      </article>
     );
   }
 }
