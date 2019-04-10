9670800a01043c097cb9473b5d9d29009fa3ac23
Mauro Colella <mauro.ludovico.colella@gmail.com>
2019-04-10 17:02:08 +0800

    Restyle header (#73)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,42 +4,12 @@ import 'picturefill';
 import ReactGA from 'react-ga';
 
 import SunIcon from '../Icons/SunIcon';
-// eslint-disable-next-line import/no-unresolved
-import coverImage from '../../assets/images/blue-peaks.jpg?sizes[]=480,sizes[]=640,sizes[]=800,sizes[]=1280,sizes[]=1600';
 
 import styles from './Header.scss';
 
 smoothscroll.polyfill();
 
 class Header extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      scrollOffset: 0,
-      parallaxSpeed: 0.05,
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll = () => {
-    const { scrollOffset } = this.state;
-    const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-
-    if (scrollTop !== scrollOffset) {
-      this.setState({
-        scrollOffset: scrollTop,
-      });
-    }
-  }
-
   handleScrollToTarget = (event) => {
     event.preventDefault();
     const target = event.target.getAttribute('href');
@@ -55,23 +25,11 @@ class Header extends PureComponent {
     });
   }
 
-
   render() {
-    const { scrollOffset, parallaxSpeed } = this.state;
-
     return (
       <header
         className={styles.header}
       >
-        <img
-          className={styles.coverImage}
-          src={coverImage.src}
-          srcSet={coverImage.srcSet}
-          alt=""
-          style={{
-            transform: `translate3d(-50%,-${50 - (scrollOffset * parallaxSpeed)}%,0)`,
-          }}
-        />
         <section className={styles.cover}>
           <h1 className={styles.cover__title}>Mauro Colella</h1>
           <a href="#footer" onClick={this.handleScrollToTarget} className={styles.hireme}>
