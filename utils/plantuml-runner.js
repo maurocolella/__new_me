8d65970328a9eb01ae4d4f1fd5d12b9e4539aac0
Mauro Colella <mauro.ludovico.colella@gmail.com>
2019-06-30 09:24:30 +0800

    Add navigation to svg

--- utils/plantuml-runner.js
+++ utils/plantuml-runner.js
@@ -2,19 +2,55 @@
 const plantuml = require('node-plantuml');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
-const getDirName = require('path').dirname;
+// const getDirName = require('path').dirname;
+const ejs = require('ejs');
 
-function writeFile(inputFile, outputFile) {
-  mkdirp(getDirName(outputFile), function parseFile(err) {
+function writeFile(inputDir, outputDir) {
+  mkdirp(outputDir, function parseFile(err) {
     if (err) {
       console.log(err);
       return;
     }
-    const gen = plantuml.generate(inputFile, { format: 'svg' });
-    gen.out.pipe(fs.createWriteStream(outputFile));
-    console.log(`${inputFile} => ${outputFile}`);
-    console.log('Successfully processed uml');
+    const gen = plantuml.generate(`${inputDir}/diagram.plantuml`, { format: 'svg' });
+    // pipe(fs.createWriteStream(outputFile));
+    const chunks = [];
+    let svg = '';
+
+    gen.out.on('data', function readData(chunk) {
+      chunks.push(chunk);
+    });
+
+    // Send the buffer or you can put it into a var
+    gen.out.on('end', function commitData() {
+      svg = Buffer.concat(chunks).toString('utf8');
+
+      ejs.renderFile(
+        `${inputDir}/viewer.ejs`,
+        { svg },
+        {},
+        function renderHTML(renderError, str) {
+          if (renderError) {
+            throw renderError;
+          }
+          fs.writeFile(`${outputDir}/viewer.html`, str, 'utf8', function afterWrite(writeError) {
+            if (writeError) {
+              throw writeError;
+            }
+            fs.copyFile(
+              `${inputDir}/svg-pan-zoom.js`,
+              `${outputDir}/svg-pan-zoom.js`, function copyScript(copyError) {
+                if (copyError) {
+                  throw copyError;
+                }
+                console.log(`${inputDir} => ${outputDir}`);
+                console.log('Successfully processed uml');
+              },
+            );
+          });
+        },
+      );
+    });
   });
 }
 
-writeFile('doc/diagram.plantuml', 'reports/diagram.svg');
+writeFile('doc/diagram', 'reports/diagram');
